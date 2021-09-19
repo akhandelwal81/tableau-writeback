d6ba2e53f28cd0da63d5e1e99f75f0f8bcc74699
akhandelwal81 <78117743+akhandelwal81@users.noreply.github.com>
2021-09-19 18:50:22 +0100

    Update writeback.service.ts

--- src/app/modules/service/writeback.service.ts
+++ src/app/modules/service/writeback.service.ts
@@ -1,11 +1,11 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {IMrkKeyPair,MrkKeyPair} from '../view-model/mrkkeypair.model';
+import {KeyPair,KeyPair} from '../view-model/keypair.model';
 import { HttpClient,HttpParams} from '@angular/common/http';
 
 Injectable({providedIn: 'root'})
 
 export class WriteBackService {
-  private writebackKey:BehaviorSubject<IMrkKeyPair> = new  BehaviorSubject<IMrkKeyPair>(new MrkKeyPair());
+  private writebackKey:BehaviorSubject<KeyPair> = new  BehaviorSubject<KeyPair>(new KeyPair());
     
 
