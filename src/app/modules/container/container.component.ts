56a57ac616225d96b33379ab07b1b69996d36962
akhandelwal81 <78117743+akhandelwal81@users.noreply.github.com>
2021-09-19 19:07:48 +0100

    Update container.component.ts

--- src/app/modules/container/container.component.ts
+++ src/app/modules/container/container.component.ts
@@ -1,25 +1,18 @@
 import { Component, OnInit, ViewChild, ElementRef, AfterContentInit,AfterViewInit} from '@angular/core';
 import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
 import {MrkKeyPair,IMrkKeyPair } from 'src/app/modules/view-models/mrkkeypair.model';
-/*  import {WritebackService} from '../services/writeback.service';
-*/
+import {WritebackService} from '../services/writeback.service';
 
 import {ComplierMetadataResolver, debugOutputAstAsTypeScript } from '@angular/compiler';
 
-/*
 import {TableauService} from 'src/app/tableau/tableau.service';
-
-*/
 
 import { Router} from '@angular/router';
 import {HttpParams,HttpClient } from '@angular/common/http';
 
 import  {Iuser} from 'src/app/service/auth/user';
 
-/*
 import { ConfigService } from 'src/app/service/config/config.service;
-
-*/
 
 declare var tableau: any;
 
