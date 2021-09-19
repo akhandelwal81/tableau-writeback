46aca793d696fb19536e45cfdb041d4a040ae6df
akhandelwal81 <78117743+akhandelwal81@users.noreply.github.com>
2021-09-19 17:50:06 +0100

    Create env.ts

---
+++ src/environment/env.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  dev: false,
+
+  authUrl: "td/auth",
+  SrvUrl: "td/comment",
+
+  dashboardUrl:
+  "http://tableau.com/Enterprise/views/Dashboard/Home?iframeSizedtoWindow=true&:embed=y&:device=desktop$:render=false%60#1"
+};
